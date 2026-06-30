d04df32ccd663dbe05c33edf024c30d3e012fb97
AshishResolute <anshugourh21@gmail.com>
2026-06-30 13:49:13 +0530

    Added functionality to the form , now the form can easily send the data to backend and the UI renders the changes in real-time updating list

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import { JobList } from "./components/JobList";
 import {useState} from 'react'
 export const App = () => {
   const [jobApplications,setJobApplications] = useState([])
+  const [jobTitle,setJobTitle]=useState('')
+  const [company,setCompany]=useState('')
+  const [source,setSource]=useState('')
+  const [date,setDate]=useState('')
   useEffect(()=>{
     const fetchJobApplications = async()=>{
       const fetchDetails =  await fetch(`http://localhost:3000/application`)
       const userJobApplications = await fetchDetails.json()
-      console.log(userJobApplications)
       setJobApplications((prevApplications)=>{
         return [...prevApplications,...userJobApplications.applications]
       })
@@ -20,7 +23,7 @@ export const App = () => {
     <>
       <Header jobApplications={jobApplications}/>
       <main id="main">
-        <JobForm />
+        <JobForm title={jobTitle} setJobTitle={setJobTitle} company={company} setCompany={setCompany} source={source} setSource={setSource} date={date} setDate={setDate} setJobApplications={setJobApplications}/>
         <JobList jobApplications={jobApplications}/>
       </main>
       
--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -1,16 +1,83 @@
-export const JobForm = ()=>{
-    return (
-        <form action="" id="jobForm">
-            <h2>Add New Application</h2>
-            <label htmlFor="title">title:</label>
-            <input type="text" id="title"/>
-            <label htmlFor="company">Company:</label>
-            <input type="text" id="company"/>
-            <label htmlFor="source">Source:</label>
-            <input type="text" id="source" placeholder="Enter Job Application URL"/>
-            <label htmlFor="applied_date">Applied On:</label>
-            <input type="date" id="applied_date"/>
-            <input type="submit" />
-        </form>
-    )
-}
+export const JobForm = ({
+  title,
+  setJobTitle,
+  company,
+  setCompany,
+  source,
+  setSource,
+  date,
+  setDate,
+  setJobApplications,
+}) => {
+  const formHandler = async (event) => {
+    event.preventDefault();
+    let formData = {
+      title,
+      company,
+      source,
+      date:new Date(date).toISOString()
+    };
+    try {
+      const response = await fetch("http://localhost:3000/application", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log(`Form data sent!`);
+        setJobApplications((prev) => {
+          return [...prev, formData];
+        });
+      }
+    } catch (error) {
+      console.error(`Form submission failed!,${error.message}`);
+    }
+  };
+  return (
+    <form action="" id="jobForm" onSubmit={formHandler}>
+      <h2>Add New Application</h2>
+      <label htmlFor="title">title:</label>
+      <input
+        type="text"
+        id="job_title"
+        value={title}
+        onChange={(event) => {
+          console.log(title);
+          setJobTitle(event.target.value);
+        }}
+      />
+      <label htmlFor="company">Company:</label>
+      <input
+        type="text"
+        id="applied_company"
+        value={company}
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+      />
+      <label htmlFor="source">Source:</label>
+      <input
+        type="text"
+        id="job_source"
+        value={source}
+        onChange={(e) => {
+          setSource(e.target.value);
+        }}
+        placeholder="Enter Job Application URL..."
+      />
+      <label htmlFor="applied_date">Applied On:</label>
+      <input
+        type="date"
+        id="applied_date"
+        value={date}
+        onChange={(event) => {
+          console.log(event.target.value);
+          setDate(event.target.value);
+        }}
+      />
+      <input type="submit" />
+    </form>
+  );
+};
